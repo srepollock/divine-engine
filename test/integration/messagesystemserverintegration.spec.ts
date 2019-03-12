9e1246075caf13ba72d80fe4a19ace2aa5cbefc9
Spencer Pollock <srepollock@gmail.com>
2019-03-11 22:56:53 -0700

    test(message system): Added some blank server tests
    This has been deemed out of scope, adding to a backlog or roadmap later on. For now, it will be
    placed here.

---
+++ test/integration/messagesystemserverintegration.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from "chai";
+
+// REVIEW: This is out of scope for the project at this time.
+/**
+ * Message system:
+ *  _local: Process
+ *  _remote: undefined | process
+ * _online: bool
+ */
+describe("Message System - Server integration", () => {
+    it("should connect to an internal node server", () => {
+        // TODO: Start server
+        // TODO: Connect to server
+        // TODO: Check connection is aqcuired
+    });
+    // TODO: Check to see that the server has incoming sunset engine messages
+    it("should get the servers messages added to it's game loop", () => {
+        
+    });
+    it("should attach to the server as a new instance", () => {
+
+    })
+    // TODO: Check to see if the server can run messages
+    it("should be able to send messages to the server", () => {
+
+    });
+});
