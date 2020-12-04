bec5f3d6f943dba33633fafcbe2ee6d874f7b472
Spencer Pollock <spencer@spollock.ca>
2020-12-04 00:25:59 -0800

    fix: 🐛 Audit fix and removing unnecessary math functions

--- src/math/index.ts
+++ src/math/index.ts
@@ -2,5 +2,5 @@ export * from "./vertex";
 export * from "./vector2";
 export * from "./vector3";
 export * from "./matrix4";
-export * from "./math";
+// export * from "./math";
 export * from "./vertex";
